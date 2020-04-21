5effd1c9aafd9b6fa9f3bd8680f9fa8431a9f008
xiebing <291362069@qq.com>
2020-04-21 17:48:36 +0800

    update

--- src/com/socket/env_monitor/LongServer.java
+++ src/com/socket/env_monitor/LongServer.java
@@ -7,10 +7,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 
-public class Server extends Thread {
+public class LongServer extends Thread {
 	Socket socket = null;
 	ServerSocket server = null;
-	public Server(int port) throws BindException {
+	public LongServer(int port) throws BindException {
 		try {
 			server = new ServerSocket(port);
 		} catch (BindException e) {
@@ -26,11 +26,11 @@ public class Server extends Thread {
 	public void run() {
 		super.run();
 		try {
-//			East.insert("服务端启动监听");
+//			System.out.println("服务端启动监听");
 			while (true) {
 				socket = server.accept();
-//				East.insert("-start【"+socket.getInetAddress().getHostAddress()+"】-");
-//				East.insert("客户端连接成功");
+//				System.out.println("-start【"+socket.getInetAddress().getHostAddress()+"】-");
+//				System.out.println("客户端连接成功");
 				new Thread(new MyRuns(socket)).start();
 			}
 		} catch (Exception e) {
@@ -60,36 +60,19 @@ public class Server extends Thread {
 				int len = 0;
 				byte[] buf = new byte[1024];
 				while ((len = in.read(buf)) != -1) {
-
-//					East.insert("收到数据：[" + new String(buf, 0, len) + "]");
 					String data_t = new String(buf, 0, len);
 					if (!data_t.matches("^[A-Fa-f0-9]+$")) {
-//						East.insert("数据非法");
 					}else {
-						SocketEntity entity = new DataTreating().treat(data_t);
-						if (entity == null) {
-							System.out.println("实体异常");
-//							East.insert("处理" + socket.getInetAddress().getHostAddress() + " 数据异常");
-							return;
-						}
-						boolean saveBeans = new DBHelper().saveBeans(entity);
-						if (!saveBeans) {
-							System.out.println("数据库处理异常");
-//							East.insert("处理" + socket.getInetAddress().getHostAddress() + " 数据库异常");
-							return;
-						}
-//						East.insert("数据结果【" + entity.getString() + "】");
+//						System.out.println("数据结果【" + entity.getString() + "】");
 					}
-//					East.insert("-stop【"+socket.getInetAddress().getHostAddress()+"】-");
+//					System.out.println("-stop【"+socket.getInetAddress().getHostAddress()+"】-");
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			} finally {
 				try {
 					if (socket != null) {
-//						East.insert(socket.getInetAddress().getHostAddress() + " 客户端关闭成功");
-//						East.btnStart.setEnabled(true);
-//						East.btnStart.setText("重新启动");
+//						System.out.println(socket.getInetAddress().getHostAddress() + " 客户端关闭成功");
 						socket.close();
 					}
 				} catch (Exception e2) {
